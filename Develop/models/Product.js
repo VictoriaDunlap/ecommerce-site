cad40e6aa0eadff4be9bc101c1627aa2b5d45c5f
Victoria Dunlap <victoriacdunlap@gmail.com>
2022-08-07 14:27:24 -0400

    category and product models columns defined

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -10,6 +10,20 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
   },
   {
     sequelize,
